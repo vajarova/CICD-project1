4583e9470758b72bd0e84c78efdc6005de89253c
neda.vazharova <neda.vazharova@gmail.com>
2022-01-02 15:19:43 +0000

    adding jenkinsfile and mvn

---
+++ src/main/java/SpringProject1forscale/SpringProject1NameApplication.java
@@ -0,0 +1,13 @@
+package SpringProject1forscale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject1NameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject1NameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SpringProject1forscale/SpringProject1NameApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringProject1forscale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject1NameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
